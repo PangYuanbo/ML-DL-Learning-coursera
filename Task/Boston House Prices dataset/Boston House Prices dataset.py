cd449073abf194e031261e937c984d629c7ba90f
PangYuanbo <147596230+PangYuanbo@users.noreply.github.com>
2024-05-02 14:37:34 -0700

    boston house

--- Task/Boston House Prices dataset/Boston House Prices dataset.py
+++ Task/Boston House Prices dataset/Boston House Prices dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -39,10 +40,11 @@ np_value = np_df[:, 13]
 np_data = np_df[:, 0:13]
 np_data = z_score_normalize(np_data)
 print(np_data.shape, np_value.shape)
+t=time.time()
 model = LinearRegression()
 model.fit(np_data, np_value)
 y_pred = model.predict(np_data)
-
+print("Time:", time.time()-t)
 # 计算均方误差
 mse = np.mean((np_value - y_pred) ** 2)
 print("MSE:", mse)
