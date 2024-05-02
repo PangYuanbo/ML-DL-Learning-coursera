92888fa0112c7efe76b0dfa5320dfeab8fd8af6b
PangYuanbo <147596230+PangYuanbo@users.noreply.github.com>
2024-05-02 13:16:47 -0700

    boston house

--- Task/Boston House Prices dataset/Boston House Prices dataset.py
+++ Task/Boston House Prices dataset/Boston House Prices dataset.py
@@ -10,7 +10,7 @@ class LinearRegression:
         self.weights = None
         self.b = None
 
-    def fit(self, X, y, batch_size=1, lr=0.01, epochs=1000):
+    def fit(self, X, y, lr=0.01, epochs=1000):
         self.X = X
         self.y = y
         n_samples, n_features = self.X.shape
@@ -26,14 +26,23 @@ class LinearRegression:
         return np.dot(X, self.weights) + self.b
 
 
+def z_score_normalize(X):
+    X_mean = np.mean(X, axis=0)
+    X_std = np.std(X, axis=0)
+    X_standardized = (X - X_mean) / X_std
+    return X_standardized
+
+
 df = pd.read_csv('boston.csv')
 np_df = np.array(df)
 np_value = np_df[:, 13]
 np_data = np_df[:, 0:13]
+np_data = z_score_normalize(np_data)
+print(np_data.shape, np_value.shape)
 model = LinearRegression()
 model.fit(np_data, np_value)
 y_pred = model.predict(np_data)
 
 # 计算均方误差
-mse = np.mean((np_value - y_pred)**2)
+mse = np.mean((np_value - y_pred) ** 2)
 print("MSE:", mse)
