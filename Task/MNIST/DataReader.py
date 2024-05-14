c68c946b661dc60ad02e75305fda9e36a0d51833
PangYuanbo <147596230+PangYuanbo@users.noreply.github.com>
2024-05-14 11:09:20 -0700

    MNIST

--- Task/MNIST/DataReader.py
+++ Task/MNIST/DataReader.py
@@ -3,34 +3,35 @@ import numpy as np
 import struct
 import os
 from array import array
+
+
 class ReadMNIST(object):
-    def __init__(self,train_images_filepath,train_labels_filepath,test_images_filepath,test_labels_filepath):
-        self.train_images_filepath=train_images_filepath
-        self.train_labels_filepath=train_labels_filepath
-        self.test_images_filepath=test_images_filepath
-        self.test_labels_filepath=test_labels_filepath
-    def read_images_labels(self,images_filepath,labels_filepath):
-        labels=[]
-        with open(labels_filepath,'rb') as file:
+    def __init__(self, train_images_filepath, train_labels_filepath, test_images_filepath, test_labels_filepath):
+        self.train_images_filepath = train_images_filepath
+        self.train_labels_filepath = train_labels_filepath
+        self.test_images_filepath = test_images_filepath
+        self.test_labels_filepath = test_labels_filepath
+
+    def read_images_labels(self, images_filepath, labels_filepath):
+        labels = []
+        with open(labels_filepath, 'rb') as file:
             magic, size = struct.unpack(">II", file.read(8))
-            if magic!=2049:
+            if magic != 2049:
                 raise ValueError('Magic number mismatch, expected 2049 but got {}'.format(magic))
-            labels=array('B',file.read())
-        with open(images_filepath,'rb') as file:
+            labels = array('B', file.read())
+        with open(images_filepath, 'rb') as file:
             magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
-            if magic!=2051:
+            if magic != 2051:
                 raise ValueError('Magic number mismatch, expected 2051 but got {}'.format(magic))
-            image_data=array('B',file.read())
-        images=[]
+            image_data = array('B', file.read())
+        images = []
         for i in range(size):
-            images.append([0]*rows*cols)
+            images.append([0] * rows * cols)
         for i in range(size):
-            images[i][:]=np.array(image_data[i*rows*cols:(i+1)*rows*cols]).reshape(rows,cols)
-        return images,labels
+            images[i][:] = np.array(image_data[i * rows * cols:(i + 1) * rows * cols]).reshape(rows, cols)
+        return images, labels
+
     def load_data(self):
-        x_train,y_train=self.read_images_labels(self.train_images_filepath,self.train_labels_filepath)
-        x_test,y_test=self.read_images_labels(self.test_images_filepath,self.test_labels_filepath)
-        return (x_train,y_train),(x_test,y_test)
-
-
-
+        x_train, y_train = self.read_images_labels(self.train_images_filepath, self.train_labels_filepath)
+        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+        return (x_train, y_train), (x_test, y_test)
