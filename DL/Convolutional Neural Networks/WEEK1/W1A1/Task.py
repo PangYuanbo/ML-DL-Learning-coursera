d2e606b83f4a5f46eed58039942538062d9582b3
PangYuanbo <147596230+pangyuanbo@users.noreply.github.com>
2024-05-19 18:44:12 -0700

    WEEK1

--- DL/Convolutional Neural Networks/WEEK1/W1A1/Task.py
+++ DL/Convolutional Neural Networks/WEEK1/W1A1/Task.py
@@ -482,3 +482,101 @@ def distribute_value(dz, shape):
     a = np.ones(shape) * average
     # YOUR CODE ENDS HERE
     return a
+a = distribute_value(2, (2, 2))
+print('distributed value =', a)
+
+
+assert type(a) == np.ndarray, "Output must be a np.ndarray"
+assert a.shape == (2, 2), f"Wrong shape {a.shape} != (2, 2)"
+assert np.sum(a) == 2, "Values must sum to 2"
+
+a = distribute_value(100, (10, 10))
+assert type(a) == np.ndarray, "Output must be a np.ndarray"
+assert a.shape == (10, 10), f"Wrong shape {a.shape} != (10, 10)"
+assert np.sum(a) == 100, "Values must sum to 100"
+
+print("\033[92m All tests passed.")
+
+
+def pool_backward(dA, cache, mode="max"):
+    """
+    Implements the backward pass of the pooling layer
+
+    Arguments:
+    dA -- gradient of cost with respect to the output of the pooling layer, same shape as A
+    cache -- cache output from the forward pass of the pooling layer, contains the layer's input and hparameters
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+
+    Returns:
+    dA_prev -- gradient of cost with respect to the input of the pooling layer, same shape as A_prev
+    """
+    # Retrieve information from cache (≈1 line)
+    # (A_prev, hparameters) = None
+    (A_prev, hparameters) = cache
+    # Retrieve hyperparameters from "hparameters" (≈2 lines)
+    # stride = None
+    # f = None
+    stride = hparameters['stride']
+    f = hparameters['f']
+    # Retrieve dimensions from A_prev's shape and dA's shape (≈2 lines)
+    # m, n_H_prev, n_W_prev, n_C_prev = None
+    # m, n_H, n_W, n_C = None
+    m, n_H_prev, n_W_prev, n_C_prev = A_prev.shape
+    m, n_H, n_W, n_C = dA.shape
+    # Initialize dA_prev with zeros (≈1 line)
+    # dA_prev = None
+    dA_prev = np.zeros(A_prev.shape)
+    # for i in range(None): # loop over the training examples
+    for i in range(m):
+        # select training example from A_prev (≈1 line)
+        # a_prev = None
+        a_prev = A_prev[i]
+        # for h in range(n_H):                   # loop on the vertical axis
+        for h in range(n_H):
+            # for w in range(n_W):               # loop on the horizontal axis
+            for w in range(n_W):
+                # for c in range(n_C):           # loop over the channels (depth)
+                for c in range(n_C):
+
+                # Find the corners of the current "slice" (≈4 lines)
+                # vert_start = None
+                # vert_end = None
+                # horiz_start = None
+                # horiz_end = None
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
+                    # Compute the backward propagation in both modes.
+                    # if mode == "max":
+                    if mode== "max":
+                        # Use the corners and "c" to define the current slice from a_prev (≈1 line)
+                        # a_prev_slice = None
+                        a_prev_slice = a_prev[vert_start:vert_end, horiz_start:horiz_end, c]
+                        # Create the mask from a_prev_slice (≈1 line)
+                        # mask = None
+                        mask = create_mask_from_window(a_prev_slice)
+                        # Set dA_prev to be dA_prev + (the mask multiplied by the correct entry of dA) (≈1 line)
+                        # dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += None
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += mask * dA[i, h, w, c]
+
+                    # elif mode == "average":
+                    elif mode == "average":
+
+                        # Get the value da from dA (≈1 line)
+                        # da = None
+                        da = dA[i, h, w, c]
+                        # Define the shape of the filter as fxf (≈1 line)
+                        # shape = None
+                        shape = (f, f)
+                        # Distribute it to get the correct slice of dA_prev. i.e. Add the distributed value of da. (≈1 line)
+                        # dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += None
+                        # YOUR CODE STARTS HERE
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += distribute_value(da, shape)
+
+                        # YOUR CODE ENDS HERE
+
+                # Making sure your output shape is correct
+    assert (dA_prev.shape == A_prev.shape)
+
+    return dA_prev
