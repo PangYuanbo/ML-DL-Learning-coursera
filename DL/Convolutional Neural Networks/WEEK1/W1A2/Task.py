f9c0e3f3f4fc97be4c8af468647120a153f45ea0
Aaron <yuanbopang@outlook.com>
2024-05-19 19:24:35 -0700

    tensorflow

--- DL/Convolutional Neural Networks/WEEK1/W1A2/Task.py
+++ DL/Convolutional Neural Networks/WEEK1/W1A2/Task.py
@@ -9,6 +9,7 @@ import pandas as pd
 import tensorflow as tf
 import tensorflow.keras.layers as tfl
 from tensorflow.python.framework import ops
+
 from cnn_utils import *
 from test_utils import summary, comparator
 
@@ -33,3 +34,113 @@ print ("Y_test shape: " + str(Y_test.shape))
 index = 124
 plt.imshow(X_train_orig[index]) #display sample training image
 plt.show()
+
+
+# GRADED FUNCTION: happyModel
+
+def happyModel():
+    """
+    Implements the forward propagation for the binary classification model:
+    ZEROPAD2D -> CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> FLATTEN -> DENSE
+
+    Note that for simplicity and grading purposes, you'll hard-code all the values
+    such as the stride and kernel (filter) sizes.
+    Normally, functions should take these values as function parameters.
+
+    Arguments:
+    None
+
+    Returns:
+    model -- TF Keras model (object containing the information for the entire training process)
+    """
+    model = tf.keras.Sequential([
+        ## ZeroPadding2D with padding 3, input shape of 64 x 64 x 3
+        tfl.ZeroPadding2D(padding=3, input_shape=(64, 64, 3)),
+        ## Conv2D with 32 7x7 filters and stride of 1
+        tfl.Conv2D(filters=32, kernel_size=(7, 7), strides=(1, 1)),
+        ## BatchNormalization for axis 3
+        tfl.BatchNormalization(axis=3),
+        ## ReLU
+        tfl.ReLU(),
+        ## Max Pooling 2D with default parameters
+        tfl.MaxPooling2D(),
+        ## Flatten layer
+        tfl.Flatten(),
+        ## Dense layer with 1 unit for output & 'sigmoid' activation
+        tfl.Dense(units=1, activation='sigmoid')
+        # YOUR CODE STARTS HERE
+
+        # YOUR CODE ENDS HERE
+    ])
+
+    return model
+
+
+happy_model = happyModel()
+# Print a summary for each layer
+for layer in summary(happy_model):
+    print(layer)
+
+output = [['ZeroPadding2D', (None, 70, 70, 3), 0, ((3, 3), (3, 3))],
+          ['Conv2D', (None, 64, 64, 32), 4736, 'valid', 'linear', 'GlorotUniform'],
+          ['BatchNormalization', (None, 64, 64, 32), 128],
+          ['ReLU', (None, 64, 64, 32), 0],
+          ['MaxPooling2D', (None, 32, 32, 32), 0, (2, 2), (2, 2), 'valid'],
+          ['Flatten', (None, 32768), 0],
+          ['Dense', (None, 1), 32769, 'sigmoid']]
+
+comparator(summary(happy_model), output)
+happy_model.compile(optimizer='adam',
+                   loss='binary_crossentropy',
+                   metrics=['accuracy'])
+happy_model.summary()
+
+
+# GRADED FUNCTION: convolutional_model
+
+def convolutional_model(input_shape):
+    """
+    Implements the forward propagation for the model:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> DENSE
+
+    Note that for simplicity and grading purposes, you'll hard-code some values
+    such as the stride and kernel (filter) sizes.
+    Normally, functions should take these values as function parameters.
+
+    Arguments:
+    input_img -- input dataset, of shape (input_shape)
+
+    Returns:
+    model -- TF Keras model (object containing the information for the entire training process)
+    """
+
+    input_img = tf.keras.Input(shape=input_shape)
+    ## CONV2D: 8 filters 4x4, stride of 1, padding 'SAME'
+    # Z1 = None
+    Z1= tfl.Conv2D(filters=8, kernel_size=(4, 4), strides=(1, 1), padding='SAME')(input_img)
+    ## RELU
+    # A1 = None
+    A1 = tfl.ReLU()(Z1)
+    ## MAXPOOL: window 8x8, stride 8, padding 'SAME'
+    # P1 = None
+    P1 = tfl.MaxPooling2D(pool_size=(8, 8), strides=(8, 8), padding='SAME')(A1)
+    ## CONV2D: 16 filters 2x2, stride 1, padding 'SAME'
+    # Z2 = None
+    Z2 = tfl.Conv2D(filters=16, kernel_size=(2, 2), strides=(1, 1), padding='SAME')(P1)
+    ## RELU
+    # A2 = None
+    A2= tfl.ReLU()(Z2)
+    ## MAXPOOL: window 4x4, stride 4, padding 'SAME'
+    # P2 = None
+    P2= tfl.MaxPooling2D(pool_size=(4, 4), strides=(4, 4), padding='SAME')(A2)
+    ## FLATTEN
+    # F = None
+    F= tfl.Flatten()(P2)
+    ## Dense layer
+    ## 6 neurons in output layer. Hint: one of the arguments should be "activation='softmax'"
+    # outputs = None
+    # YOUR CODE STARTS HERE
+    outputs = tfl.Dense(units=6, activation='softmax')(F)
+    # YOUR CODE ENDS HERE
+    model = tf.keras.Model(inputs=input_img, outputs=outputs)
+    return model
